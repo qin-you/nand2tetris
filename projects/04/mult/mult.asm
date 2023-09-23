039923f7f1a26993cc99a66fa2f825387a7b09fd
qinyou <MG20150006@smail.nju.edu.cn>
2023-09-23 10:19:24 +0800

    project 04

--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -1,9 +1,33 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Mult.asm
-
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+
+
+// R2 = R0+R0+R0+..   number R1 of R0s add
+
+@2
+M = 0	// R2 = 0
+
+@0
+D = M
+@END
+D;JEQ		// if R0==0, need not calculate
+
+(LOOP)		// let LOOP signs address of next line
+@1
+M = M - 1	// R1 -= 1
+D = M
+@END
+D;JLT		// if R1 < 0: jmp  END.   unique exit of this loop
+
+@0
+D = M
+@2
+M = M + D	// R2 += R0
+
+@LOOP
+0;JMP		// loop
+
+
+(END)
+0;JMP
